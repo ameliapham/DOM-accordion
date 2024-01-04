0c94d040719770360ca8b567326b0c3697c890d4
ameliapham <ptt.huong.8895@gmail.com>
2024-01-04 23:33:27 +0100

    Correct the target of the click event

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 import './style.css'
 
 document.querySelector('.accordion').addEventListener('click', (e) => {
-    if(e.target && e.target.matches('.question')){
-        let hiddenAnswer = e.target.nextElementSibling
+    let target = e.target
+    if (target.matches('p')){
+        target = target.parentElement
+    }
+    if(target.matches('.question')){
+        let hiddenAnswer = target.nextElementSibling
         hiddenAnswer.classList.toggle('show')
-
     }
 })
