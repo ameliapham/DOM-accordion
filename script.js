e368ce611291e598e281836ffbed3180cc9c5519
ameliapham <ptt.huong.8895@gmail.com>
2024-01-04 21:15:59 +0100

    Add function accordion

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+document.querySelector('.accordion').addEventListener('click', (e) => {
+    if(e.target && e.target.matches('.question')){
+        let hiddenAnswer = e.target.nextElementSibling
+        hiddenAnswer.classList.toggle('show')
+
+    }
+})
