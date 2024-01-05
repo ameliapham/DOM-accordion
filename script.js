50d7b90be385d402cb0992c0fcf2306a012ced1e
ameliapham <ptt.huong.8895@gmail.com>
2024-01-05 01:27:26 +0100

    Change the icon library to Remix Icon

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ import './style.css'
 
 document.querySelector('.accordion').addEventListener('click', (e) => {
     let target = e.target
-    if (target.matches('p')){
+    if (target.matches('p') || target.matches('i')){
         target = target.parentElement
     }
     if(target.matches('.question')){
         let hiddenAnswer = target.nextElementSibling
         hiddenAnswer.classList.toggle('show')
 
-        let icon = document.querySelector('.icon')
+        let icon = target.querySelector('i')
         if (hiddenAnswer.classList.contains('show')){
-            icon.innerText = '-'
+            icon.className = 'ri-subtract-line'
         } else {
-            icon.innerText = '+'
+            icon.className = 'ri-add-fill'
         }
     }
 })
